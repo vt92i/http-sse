09e415b8e3b8ede8b63290083da0e1690591b778
fikaa <benidiktusvio@gmail.com>
2023-05-22 03:08:29 +0700

    🐛 fix(http-sse.go): add error handling for invalid and unreachable URLs ✨ feat(http-sse.go): add pingTest function to handle ping tests and improve code readability The code now handles invalid and unreachable URLs by returning an appropriate error message. The pingTest function was added to handle ping tests and improve code readability. The function takes a channel and URL as parameters and runs a continuous ping test on the URL. The ping data is then sent to the channel for processing. The log message now includes the ping time and the client's IP address.

--- http-sse.go
+++ http-sse.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -12,25 +13,57 @@ type StreamingData struct {
 	Message string `json:"message"`
 }
 
-func main() {
-	ping := make(chan float64)
+func pingTest(c chan float64, URL string) {
+	for {
+		timeStart := time.Now()
 
-	go func() {
-		for {
-			var timeStart, timeEnd time.Time
-			timeStart = time.Now()
-
-			_, err := http.Get("https://kuliah.uajy.ac.id")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			timeEnd = time.Now()
-			ping <- timeEnd.Sub(timeStart).Seconds() * 1000
+		if _, err := http.Get(URL); err != nil {
+			log.Println(err)
+			return
 		}
-	}()
 
+		c <- time.Now().Sub(timeStart).Seconds() * 1000
+	}
+}
+
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Get URL parameter
+		URL := r.URL.Query().Get("url")
+		if len(URL) == 0 {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "Missing URL parameter.",
+			})
+			return
+		}
+
+		// Check if URL is valid
+		if _, err := url.ParseRequestURI(URL); err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "Invalid URL parameter.",
+			})
+			return
+		}
+
+		// Check if URL is reachable
+		if _, err := http.Get(URL); err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "URL is unreachable.",
+			})
+			return
+		}
+
+		// Create channel
+		c := make(chan float64)
+		// Start ping test
+		go pingTest(c, URL)
+
 		// Set the headers for Server-Sent Events
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "text/event-stream")
@@ -42,31 +75,28 @@ func main() {
 			flusher.Flush()
 		}
 
-		for ping := range ping {
-			data := StreamingData{
-				Message: fmt.Sprintf("%f ms", ping),
-			}
-
-			jsonData, err := json.Marshal(data)
-			if err != nil {
+		// Listen for ping data
+		for ping := range c {
+			// Send ping data to client as JSON if no error occurs
+			if jsonData, err := json.Marshal(StreamingData{Message: fmt.Sprintf("%f ms", ping)}); err != nil {
 				log.Println(err)
 				return
-			}
-
-			event := fmt.Sprintf("data: %s\n\n", jsonData)
-			_, err = w.Write([]byte(event))
-			if err != nil {
-				log.Println(err)
-				return
+			} else {
+				if _, err = w.Write([]byte(fmt.Sprintf("data: %s\n\n", jsonData))); err != nil {
+					log.Println(err)
+					return
+				}
 			}
 
 			// Flush the response buffer after each write
 			if flusher, ok := w.(http.Flusher); ok {
 				flusher.Flush()
-				log.Println(r.RemoteAddr, data.Message)
+				log.Println(fmt.Sprintf("write tcp %s->%s: write: %f ms", r.Host, r.RemoteAddr, ping))
 			}
 
 		}
 	})
+	// Start server
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Server started on port 8080.")
 }
